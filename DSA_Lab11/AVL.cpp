c6822eec6ba6c1c2d44226c77c8ce0f79557565e
Muhammad Mudassir <mudassirnaveed303@gmail.com>
2023-11-21 22:41:32 +0500

    Commit by author

---
+++ DSA_Lab11/AVL.cpp
@@ -0,0 +1,363 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    int height;
+    Node* parent;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data=data;
+        this->parent=NULL;
+        this->left=NULL;
+        this->right=NULL;
+        this->height = 1;
+    }
+    ~Node(){
+        delete parent;
+        delete left;
+        delete right;
+        this->parent=NULL;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+class BST{
+    private:
+    public:
+    Node* root=NULL;
+    BST(){
+    }
+    BST(int arr[],int size){
+        for (int i=0;i<size;i++){
+            Insert(arr[i]);
+        }
+    }
+    ~BST(){
+        root=NULL;
+    }
+    bool isEmpty(){
+        if (root==NULL){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    Node* getTree(){
+        return root;
+    }
+    void Insert(int data){
+        Node *z =new Node(data);
+        Node *y=NULL; //Trailing pointer
+        Node *x=root; //Parent of BST
+        while(x!=NULL){
+            y=x;
+            if(z->data < x->data){
+                x=x->left;
+            }
+            else{
+                x=x->right;
+            }
+        }
+        z->parent=y; //Make the parent
+        if(y==NULL){
+            root=z;
+        }
+        else if(z->data < y->data){
+            y->left=z;
+        }
+        else{
+        y->right=z;
+        }
+    }
+    Node* findNode(int data){
+        Node* x=root;
+        while(x!=NULL && x->data!=data){
+            if(data<x->data){
+                x=x->left;
+            }
+            else{
+                x=x->right;
+            }
+        }
+        return x;
+    }
+    bool deleteNode(int x){
+        Node* z=findNode(x);
+        if(z==NULL){
+            return false;
+        }
+        else{
+            if(z->left==NULL){
+                transplant(z,z->right);
+            }
+            else if(z->right==NULL){
+                transplant(z,z->left);
+            }
+            else{
+                Node* y=treeMinimum(z->right);
+                if(y->parent!=z){
+                    transplant(y,y->right);
+                    y->right=z->right;
+                    y->right->parent=y;
+                }
+                transplant(z,y);
+                y->left=z->left;
+                y->left->parent=y;
+            }
+            delete z;
+            return true;
+        }
+    }
+    void transplant(Node* u,Node* v){
+        if(u->parent==NULL){
+            root=v;
+        }
+        else if(u==u->parent->left){
+            u->parent->left=v;
+        }
+        else{
+            u->parent->right=v;
+        }
+        if(v!=NULL){
+            v->parent=u->parent;
+        }
+    }
+    Node* treeMinimum(Node* x){
+        while(x->left!=NULL){
+            x=x->left;
+        }
+        return x;
+    }
+
+    void inOrderTraversal(Node *p){
+        if(p!=NULL){
+            inOrderTraversal(p->left);
+            cout<<p->data<<" ";
+            inOrderTraversal(p->right);
+        }
+        cout<<endl;
+    }
+    void preOrderTraversal(Node *p){
+        if(p!=NULL){
+            cout<<p->data<<" ";
+            preOrderTraversal(p->left);
+            preOrderTraversal(p->right);
+        }
+        cout<<endl;
+    }
+    void postOrderTraversal(Node *p){
+        if(p!=NULL){
+            postOrderTraversal(p->left);
+            postOrderTraversal(p->right);
+            cout<<p->data<<" ";
+        }
+        cout<<endl;
+    }
+    int numberOfNodes(Node *p){
+        if(p==NULL){
+            return 0;
+        }
+        else{
+            return numberOfNodes(p->left)+numberOfNodes(p->right)+1;
+        }
+    }
+    int height(Node* T){
+        if(T==NULL){
+            return 0;
+        }
+        else{
+            int lheight=height(T->left);
+            int rheight=height(T->right);
+            if(lheight>rheight){
+                return lheight+1;
+            }
+            else{
+                return rheight+1;
+            }
+        }
+    }
+    bool isBST(Node *T){
+        if(T==NULL){
+            return true;
+        }
+        else{
+            if(T->left!=NULL && T->left->data>T->data){
+                return false;
+            }
+            if(T->right!=NULL && T->right->data<T->data){
+                return false;
+            }
+            if(!isBST(T->left) || !isBST(T->right)){
+                return false;
+            }
+            return true;
+        }
+    }
+    void leafNodes(Node *T){
+        if(T!=NULL){
+            if(T->left==NULL && T->right==NULL){
+                cout<<T->data<<" ";
+            }
+            leafNodes(T->left);
+            leafNodes(T->right);
+        }
+    }
+    void visualizeTree(Node *T,int space){
+        if(T==NULL){
+            return;
+        }
+        space+=10;
+        visualizeTree(T->right,space);
+        cout<<endl;
+        for(int i=10;i<space;i++){
+            cout<<" ";
+        }
+        cout<<T->data<<endl;
+        visualizeTree(T->left,space);
+    }  
+};
+class AVLTree:BST{
+    private:
+    public:
+    Node* root=NULL;
+    AVLTree(){
+
+    }
+    AVLTree(int arr[],int size){
+        for (int i=0;i<size;i++){
+            Insert(arr[i]);
+        }
+    }
+    ~AVLTree(){
+        delete root;
+    }
+    Node* getTree(){
+        return root;
+    }
+    int height(Node* root){
+        if(root==NULL){
+            return 0;
+        }
+        return root->height;
+    }
+    void leftRotate(Node*x){
+        Node *y=x->right;
+        x->right=y->left;
+        if(y->left!=NULL){
+            y->left->parent=x;
+        }
+        y->parent=x->parent;
+        if(x->parent==NULL){
+            root=y;
+        }
+        else if(x==x->parent->left){
+            x->parent->left=y;
+        }
+        else{
+            x->parent->right=y;
+        }
+        y->left=x;
+        x->parent=y;
+    }
+    void rightRotate(Node*x){
+        Node *y=x->left;
+        x->left=y->right;
+        if(y->right!=NULL){
+            y->right->parent=x;
+        }
+        y->parent=x->parent;
+        if(x->parent==NULL){
+            root=y;
+        }
+        else if(x==x->parent->right){
+            x->parent->right=y;
+        }
+        else{
+            x->parent->left=y;
+        }
+        y->right=x;
+        x->parent=y;
+    }
+    void balanceAVLTree(Node *T){
+        if(T==NULL){
+            return;
+        }
+        else{
+            T->height=max(height(T->left),height(T->right))+1;
+            int balanceFactor=height(T->left)-height(T->right);
+            if(balanceFactor>1){
+                if(height(T->left->left)>=height(T->left->right)){
+                    rightRotate(T);
+                }
+                else{
+                    leftRotate(T->left);
+                    rightRotate(T);
+                }
+            }
+            else if(balanceFactor<-1){
+                if(height(T->right->right)>=height(T->right->left)){
+                    leftRotate(T);
+                }
+                else{
+                    rightRotate(T->right);
+                    leftRotate(T);
+                }
+            }
+            balanceAVLTree(T->parent);
+        }
+    }
+    void insertAVL(int data){
+        Node *z =new Node(data);
+        Node *y=NULL;
+        Node *x=root;
+        while(x!=NULL){
+            y=x;
+            if(z->data<x->data){
+                x=x->left;
+            }
+            else{
+                x=x->right;
+            }
+        }
+        z->parent=y;
+        if(y==NULL){
+            root=z;
+        }
+        else if(z->data<y->data){
+            y->left=z;
+        }
+        else{
+            y->right=z;
+        }
+        //z->height=1;
+        balanceAVLTree(z);
+    }
+    void visualizeTree(Node *T,int space){
+        if(T==NULL){
+            return;
+        }
+        space+=10;
+        visualizeTree(T->right,space);
+        cout<<endl;
+        for(int i=10;i<space;i++){
+            cout<<" ";
+        }
+        cout<<T->data<<endl;
+        visualizeTree(T->left,space);
+    }  
+    
+   
+};
+int main(){
+    AVLTree t;
+    t.insertAVL(10);
+    t.insertAVL(20);
+    t.insertAVL(30);
+    t.visualizeTree(t.root,0);
+
+}
---
+++ DSA_Lab8/Part1.cpp
@@ -0,0 +1,253 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* parent;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data=data;
+        this->parent=NULL;
+        this->left=NULL;
+        this->right=NULL;
+    }
+    ~Node(){
+        delete parent;
+        delete left;
+        delete right;
+        this->parent=NULL;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+class BST{
+    private:
+    Node* root=NULL;
+    public:
+    BST(){
+    }
+    BST(int arr[],int size){
+        for (int i=0;i<size;i++){
+            Insert(arr[i]);
+        }
+    }
+    ~BST(){
+        root=NULL;
+    }
+    bool isEmpty(){
+        if (root==NULL){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    Node* getTree(){
+        return root;
+    }
+    void Insert(int data){
+        Node *z =new Node(data);
+        Node *y=NULL; //Trailing pointer
+        Node *x=root; //Parent of BST
+        while(x!=NULL){
+            y=x;
+            if(z->data < x->data){
+                x=x->left;
+            }
+            else{
+                x=x->right;
+            }
+        }
+        z->parent=y; //Make the parent
+        if(y==NULL){
+            root=z;
+        }
+        else if(z->data < y->data){
+            y->left=z;
+        }
+        else{
+        y->right=z;
+        }
+    }
+    Node* findNode(int data){
+        Node* x=root;
+        while(x!=NULL && x->data!=data){
+            if(data<x->data){
+                x=x->left;
+            }
+            else{
+                x=x->right;
+            }
+        }
+        return x;
+    }
+    bool deleteNode(int x){
+        Node* z=findNode(x);
+        if(z==NULL){
+            return false;
+        }
+        else{
+            if(z->left==NULL){
+                transplant(z,z->right);
+            }
+            else if(z->right==NULL){
+                transplant(z,z->left);
+            }
+            else{
+                Node* y=treeMinimum(z->right);
+                if(y->parent!=z){
+                    transplant(y,y->right);
+                    y->right=z->right;
+                    y->right->parent=y;
+                }
+                transplant(z,y);
+                y->left=z->left;
+                y->left->parent=y;
+            }
+            delete z;
+            return true;
+        }
+    }
+    void transplant(Node* u,Node* v){
+        if(u->parent==NULL){
+            root=v;
+        }
+        else if(u==u->parent->left){
+            u->parent->left=v;
+        }
+        else{
+            u->parent->right=v;
+        }
+        if(v!=NULL){
+            v->parent=u->parent;
+        }
+    }
+    Node* treeMinimum(Node* x){
+        while(x->left!=NULL){
+            x=x->left;
+        }
+        return x;
+    }
+
+    void inOrderTraversal(Node *p){
+        if(p!=NULL){
+            inOrderTraversal(p->left);
+            cout<<p->data<<" ";
+            inOrderTraversal(p->right);
+        }
+        cout<<endl;
+    }
+    void preOrderTraversal(Node *p){
+        if(p!=NULL){
+            cout<<p->data<<" ";
+            preOrderTraversal(p->left);
+            preOrderTraversal(p->right);
+        }
+        cout<<endl;
+    }
+    void postOrderTraversal(Node *p){
+        if(p!=NULL){
+            postOrderTraversal(p->left);
+            postOrderTraversal(p->right);
+            cout<<p->data<<" ";
+        }
+        cout<<endl;
+    }
+    int numberOfNodes(Node *p){
+        if(p==NULL){
+            return 0;
+        }
+        else{
+            return numberOfNodes(p->left)+numberOfNodes(p->right)+1;
+        }
+    }
+    int height(Node* T){
+        if(T==NULL){
+            return 0;
+        }
+        else{
+            int lheight=height(T->left);
+            int rheight=height(T->right);
+            if(lheight>rheight){
+                return lheight+1;
+            }
+            else{
+                return rheight+1;
+            }
+        }
+    }
+    bool isBST(Node *T){
+        if(T==NULL){
+            return true;
+        }
+        else{
+            if(T->left!=NULL && T->left->data>T->data){
+                return false;
+            }
+            if(T->right!=NULL && T->right->data<T->data){
+                return false;
+            }
+            if(!isBST(T->left) || !isBST(T->right)){
+                return false;
+            }
+            return true;
+        }
+    }
+    void leafNodes(Node *T){
+        if(T!=NULL){
+            if(T->left==NULL && T->right==NULL){
+                cout<<T->data<<" ";
+            }
+            leafNodes(T->left);
+            leafNodes(T->right);
+        }
+    }
+    bool isSparseTree(Node *T){
+        if(T==NULL){
+            return true;
+        }
+        else{
+            if(T->left==NULL && T->right==NULL){
+                return true;
+            }
+            if(T->left!=NULL && T->right!=NULL){
+                return false;
+            }
+            return isSparseTree(T->left) && isSparseTree(T->right);
+        }
+    }
+    void visualizeTree(Node *T,int space){
+        if(T==NULL){
+            return;
+        }
+        space+=10;
+        visualizeTree(T->right,space);
+        cout<<endl;
+        for(int i=10;i<space;i++){
+            cout<<" ";
+        }
+        cout<<T->data<<endl;
+        visualizeTree(T->left,space);
+    }  
+};
+int main(){
+    BST tree;
+    int arr[]={10,5,15,3,7,12,18,1,4,6,8,11,13,16,20};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    BST tree2(arr,size);
+    tree2.inOrderTraversal(tree2.getTree());
+    tree2.preOrderTraversal(tree2.getTree());
+    tree2.postOrderTraversal(tree2.getTree());
+    cout<<"Number of nodes: "<<tree2.numberOfNodes(tree2.getTree())<<endl;
+    cout<<"Height of the tree: "<<tree2.height(tree2.getTree())<<endl;
+    cout<<"Is the tree a BST: "<<tree2.isBST(tree2.getTree())<<endl;
+    cout<<"Leaf nodes: ";
+    tree2.leafNodes(tree2.getTree());
+    cout<<endl;
+    cout<<"Is the tree a sparse tree: "<<tree2.isSparseTree(tree2.getTree())<<endl;
+    tree2.visualizeTree(tree2.getTree(),0);
+    tree2.deleteNode(10);
+    tree2.visualizeTree(tree2.getTree(),0);
+}
